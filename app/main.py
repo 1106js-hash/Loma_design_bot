a1f314f3225983b6c61efe26f9f11a09a192ba35
Ivan Sakal <ivanivan@MacBook-Air-Ivan.local>
2026-02-12 18:31:11 +0300

    add logger and aiohttp for max

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 import asyncio
+from app.core.logger import setup_logger, get_logger
 from app.platforms.telegram.bot import run_telegram_bot
 from app.platforms.max.bot import run_max_bot
 
 
 async def main():
-    print("🤖 Боты запускаются...")
+    setup_logger()
+    logger = get_logger("main")
+
+    logger.info("🤖 Боты запускаются...")
+
     await asyncio.gather(
         run_telegram_bot(),
         run_max_bot()
@@ -13,3 +18,4 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
+
